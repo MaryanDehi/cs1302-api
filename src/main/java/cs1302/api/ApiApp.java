9a103653857955a8b4764d9a542e463bb2d399da
Tharushika Dehipitiarachchi <dtd81255@uga.edu>
2023-04-29 22:01:41 -0400

    Added some UI elements

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -1,5 +1,18 @@
 package cs1302.api;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
@@ -10,12 +23,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
+ * Lets users search for a artist, album or track and recieve recommendations based off
+ * their choice.
  */
 public class ApiApp extends Application {
     Stage stage;
     Scene scene;
     VBox root;
+
+    /** HTTP client. */
+    public static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)           // uses HTTP protocol version 2 where possible
+        .followRedirects(HttpClient.Redirect.NORMAL)  // always redirects, except from HTTPS to HTTP
+        .build();                                     // builds and returns a HttpClient object
+
+    /** Google {@code Gson} object for parsing JSON-formatted strings. */
+     public static Gson GSON = new GsonBuilder()
+         .setPrettyPrinting()                          // enable nice output when printing
+         .create();                                    // builds and returns a Gson object
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
